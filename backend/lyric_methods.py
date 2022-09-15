85607bf30324c56018a402d8855776f00cd8164c
Gabriel Dong <gabrielhdong@gmail.com>
2022-09-15 01:56:01 -0500

    Adding files

---
+++ backend/lyric_methods.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+
+
+dataset = pd.read_csv(r"C:/Users/Gabriel\Dropbox/PC\Documents/Projects/ChangePP/Challenge/ChangePP-Challenge-Repo/taylor_swift_lyrics.csv")
+
+print(dataset.to_string)
+
+def getLyric():
+    
+    strLyric = ""
+
+    return strLyric
